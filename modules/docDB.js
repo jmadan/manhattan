022c37192b95d3bb3d909d8248ba80859c6fef49
Jasdeep Madan <jasdeepm@gmail.com>
2016-07-20 21:39:56 +0530

    implemented check for the connection state before creating a new connection

--- modules/docDB.js
+++ modules/docDB.js
@@ -4,9 +4,14 @@ var mongoose = require('mongoose');
 
 exports.connectDB = () => {
 	console.log(process.env.MANHATTANDB);
-	mongoose.connect(process.env.MANHATTANDB);
-	console.log("state: ", mongoose.connection.readyState);
-	console.log("Mongo Connected");
+	console.log("before state: ", mongoose.connection.readyState);
+	if (mongoose.connection.readyState != 2) {
+		mongoose.connect("mongodb://127.0.0.1:27017/niteowl");
+		console.log("after state: ", mongoose.connection.readyState);
+		console.log("Mongo Connected");
+	} else {
+		console.log("Mongo Already Connected");
+	}
 }
 exports.disconnectDB = () => {
 	mongoose.disconnect();
