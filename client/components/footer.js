cf945268733b73a263b5f8bdf25d0fa16f6e3f65
Jasdeep Madan <jasdeepm@gmail.com>
2016-08-09 17:50:56 +0530

    components for home and others....trying it out

---
+++ client/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = React.createClass({
+	render: function() {
+		return (<div>
+			<footer>
+				<p className="text-center">Copyright-2016 - @Manhattan</p>
+			</footer>
+			</div>);		
+	}
+});
+
+export default Footer;
---
+++ client/components/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let Home = React.createClass({
+	render: function(){
+		return (<div className="jumbotron"><h1>Hello, world!</h1><p>Lets begin...</p></div>);
+	}
+});
+
+export default Home;
