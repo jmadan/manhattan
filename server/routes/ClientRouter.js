8cdcc44973cef91138f68476a1a448a96c6397ff
Jasdeep Madan <jasdeepm@gmail.com>
2016-09-28 22:15:35 +0530

    created empty endpoints and userfeed endpoint based on the interest of the user

--- server/routes/ClientRouter.js
+++ server/routes/ClientRouter.js
@@ -7,46 +7,48 @@ let Q = require('q');
 
 
 ClientRouter.use((req, res, next) => {
-	console.log('Route - time: ', Date.now());
-	console.log(req.path);
-	next();
+    console.log('Route - time: ', Date.now());
+    console.log(req.path);
+    next();
 });
 
 ClientRouter.get('/', (req, res) => {
-	res.json({message: "Need to call specific end points to get data", error: true});
+    res.json({ message: "Need to call specific end points to get data", error: true });
 });
 
 ClientRouter.get('/user/:userEmail', (req, res) => {
-	let storyFeed = [];
-	userModule.userInfo(req.params.userEmail).then((person) => {
-		console.log("person*** ", person);
-			person.interests.map((interest) => {
-				Feed.getFeed(interest).then((feed) => {storyFeed.concat(feed);});
-				return storyFeed;
-			}).then(() => {
-				res.json({user: person, message: "user info", success: true, feed: storyFeed});
-			});
-	});
+    userModule.userInfo(req.params.userEmail).then((person) => {
+        return person;
+    }).then((person) => {
+        return getUserFeedBasedOnInterest(person.interests)
+        .then((feed) => res.json({ feed: feed, message: "user feed", success: true, user: person }));
+    });
 });
 
 ClientRouter.get('/user/:userid/getfeed', (req, res) => {
-	let user = userModule.userInfo(req.params.userid);
-	res.json({feed: {}, success: true, message: "User specific feed"});
+    let user = userModule.userInfo(req.params.userid);
+    res.json({ feed: {}, success: true, message: "User specific feed" });
 });
 
 ClientRouter.post('/user/saveinterest', (req, res) => {
-	res.json({message: "User interest saved!", success: true, user: {}});
+    res.json({ message: "User interest saved!", success: true, user: {} });
 });
 
 ClientRouter.post('/user/updateinterest', (req, res) => {
-	res.json({message: "User interest updated!", success: true, user: {}});
+    res.json({ message: "User interest updated!", success: true, user: {} });
 });
 
-ClientRouter.post('/user',(req, res)=>{
-	userModule.createUser('Jasdeep', 'Madan', 'jasdeepm@gmail.com').then((person, err) => {
-		console.log("person*** ", person);
-		res.json({message: "User saved!", success: true, user: person});
-	});
+ClientRouter.post('/user', (req, res) => {
+    userModule.createUser('Jasdeep', 'Madan', 'jasdeepm@gmail.com').then((person, err) => {
+        console.log("person*** ", person);
+        res.json({ message: "User saved!", success: true, user: person });
+    });
 });
+
+function getUserFeedBasedOnInterest(interests) {
+    console.log("*****interests", interests.toString());
+    return Feed.getFeed(interests.toString());
+}
+
 
 module.exports = ClientRouter;
