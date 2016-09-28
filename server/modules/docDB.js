0f9ddba258e41746d07a43b6a67de43f9ec688d6
Jasdeep Madan <jasdeepm@gmail.com>
2016-09-28 22:05:50 +0530

    logging different state of the DB connection

--- server/modules/docDB.js
+++ server/modules/docDB.js
@@ -3,18 +3,19 @@
 var mongoose = require('mongoose');
 
 exports.connectDB = () => {
-	console.log(process.env.MANHATTANDB);
-	console.log("before state: ", mongoose.connection.readyState);
-	if (mongoose.connection.readyState != 2) {
-		mongoose.connect("mongodb://127.0.0.1:27017/niteowl");
-		console.log("after state: ", mongoose.connection.readyState);
-		console.log("Mongo Connected");
-	} else {
-		console.log("Mongo Already Connected");
-	}
+    console.log(process.env.MANHATTANDB);
+    console.log("before state: ", mongoose.connection.readyState);
+    if (mongoose.connection.readyState != 2) {
+        // mongoose.connect("mongodb://127.0.0.1:27017/niteowl");
+        mongoose.connect(process.env.MANHATTANDB);
+        console.log("after state: ", mongoose.connection.readyState);
+        console.log("Mongo Connected");
+    } else {
+        console.log("Mongo Already Connected");
+    }
 }
 exports.disconnectDB = () => {
-	mongoose.disconnect();
-	console.log("state: ", mongoose.connection.readyState);
-	console.log("Mongo Diconnected");
+    mongoose.disconnect();
+    console.log("state: ", mongoose.connection.readyState);
+    console.log("Mongo Diconnected");
 }
