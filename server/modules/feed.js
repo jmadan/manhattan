76c65a299fa38d630018b3fa224ee5861a16444f
Jasdeep Madan <jasdeepm@gmail.com>
2016-09-28 22:07:46 +0530

    refactored to do a text index search on the category with multiple keywords separated by comma

--- server/modules/feed.js
+++ server/modules/feed.js
@@ -1,14 +1,21 @@
 import Story from '../models/story';
 let Q = require('q');
 
-export function getFeed(interest) {
+export function getFeed(interests) {
 	let deferred = Q.defer();
-	Story.find({category: {$regex: interest}}, 'storyId title url category').exec((err, stories) => {
+	Story.find({$text : {$search: interests}}, 'storyId title url category').exec((err, stories) => {
 		if(err) {
 			deferred.reject(err);
 		} else {
 			deferred.resolve(stories);
 		}
 	});
+	// Story.find({category: {$regex: interest, $options: 'i'}}, 'storyId title url category').exec((err, stories) => {
+	// 	if(err) {
+	// 		deferred.reject(err);
+	// 	} else {
+	// 		deferred.resolve(stories);
+	// 	}
+	// });
 	return deferred.promise;
 };
