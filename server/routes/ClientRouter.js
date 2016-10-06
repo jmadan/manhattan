3f6abaa7898c6d9fbfb9922d686bf02a15594cd7
Jasdeep Madan <jasdeepm@gmail.com>
2016-10-06 17:25:37 +0530

    Implemented user feed based on the email of the user, create a user, fetch user based on the email

--- server/routes/ClientRouter.js
+++ server/routes/ClientRouter.js
@@ -16,18 +16,45 @@ ClientRouter.get('/', (req, res) => {
     res.json({ message: "Need to call specific end points to get data", error: true });
 });
 
-ClientRouter.get('/user/:userEmail', (req, res) => {
-    userModule.userInfo(req.params.userEmail).then((person) => {
-        return person;
-    }).then((person) => {
-        return getUserFeedBasedOnInterest(person.interests)
-        .then((feed) => res.json({ feed: feed, message: "user feed", success: true, user: person }));
+ClientRouter.get('/user/all', (req, res) => {
+    userModule.getUsers().then((persons) => {
+        res.json({ users: persons, message: "user feed", success: true});
     });
 });
 
-ClientRouter.get('/user/:userid/getfeed', (req, res) => {
-    let user = userModule.userInfo(req.params.userid);
-    res.json({ feed: {}, success: true, message: "User specific feed" });
+ClientRouter.post('/user/create', (req, res) => {
+    userModule.createUser(req.body.firstName, req.body.lastName, req.body.email).then((person, err) => {
+        console.log("person*** ", person);
+        res.json({ message: "User saved!", success: true, user: person });
+    });
+});
+
+ClientRouter.get('/user/:userEmail', (req, res) => {
+    userModule.userInfo(req.params.userEmail)
+    .then((person) => {
+        if(person == null){
+            throw new Error("Can't find the user");
+        }
+        res.json({ message: "user fetched", success: true, user: person });
+    })
+    .catch((error) => {
+        res.json({ message: error.message, success: false });  
+    });
+});
+
+ClientRouter.get('/user/feed/:userEmail', (req, res) => {
+    userModule.userInfo(req.params.userEmail).then((person) => {
+        if(person == null){
+            throw new Error("Can't find the user");
+        }
+        return person;
+    }).then((person) => {
+        return getUserFeedBasedOnInterest(person.interests)
+        .then((feed) => res.json({ feed: feed, message: "user specific feed", success: true, user: person }));
+    }).catch((err) => {
+
+        res.json({ feed: {}, message: err.message, success: false })
+    });
 });
 
 ClientRouter.post('/user/saveinterest', (req, res) => {
@@ -38,12 +65,7 @@ ClientRouter.post('/user/updateinterest', (req, res) => {
     res.json({ message: "User interest updated!", success: true, user: {} });
 });
 
-ClientRouter.post('/user', (req, res) => {
-    userModule.createUser('Jasdeep', 'Madan', 'jasdeepm@gmail.com').then((person, err) => {
-        console.log("person*** ", person);
-        res.json({ message: "User saved!", success: true, user: person });
-    });
-});
+
 
 function getUserFeedBasedOnInterest(interests) {
     console.log("*****interests", interests.toString());
