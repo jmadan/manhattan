e4ae0547a624e5f615225ce18e79e093644fe728
Jasdeep Madan <jmadan@protonmail.com>
2017-06-25 18:35:41 +1000

    WIP added methods to gt initial feed, get data from Hn based on ID and then get body of the article using URL

--- server/modules/hnfeed.js
+++ server/modules/hnfeed.js
@@ -1,37 +1,98 @@
 let fetch = require('isomorphic-fetch');
 let docDB = require('./docDB');
+let async = require('async');
 
-let checkIdExists = (mcollection, id) => {
+exports.gettext = (item) => {
   docDB.open().then((db) => {
-    return db.collection(mcollection);
+    return db.collection("hnfeed_initial");
   }).then((mcoll) => {
-    return mcoll.find({id: id}).toArray();
-  }).then((doc) => {
-    docDB.close();
-    if(doc.length > 0) {
-      return true;
+    return mcoll.find({status: "getBody"}).limit(1).toArray();
+  }).then((docs) => {
+    docs.map((doc) => {
+      fetch(doc.url).then((response)=>{
+        if(response.status >= 400){
+          throw new Error("Got fucked....");
+        }
+        return response.json();
+      }).then((details) => {
+        console.log("doc :", doc.url, "--------", details);
+      }).catch((err)=>{
+        console.log("Error updating: ", err)
+      });
+    })
+  }).catch((err)=>{
+    console.log("Error updating: ", err);
+  });
+}
+
+let updateitem = (doc) => {
+  docDB.open().then((db) => {
+    return db.collection("hnfeed_initial");
+  }).then((mcoll) => {
+    if(doc.type == 'story'){
+      return mcoll.updateOne({'hnid': doc.id}, {$set: {status: "getBody", title: doc.title, url: doc.url, type: doc.type}});
+    } else {
+      return mcoll.updateOne({'hnid': doc.id}, {$set: {type: doc.type}});
     }
+  }).then((r) => {
+    console.log("document update: ", r.modifiedCount);
+  }).catch((err)=>{
+    console.log("Error updating: ", err);
+  });
+}
+
+exports.getfeeditemdetails = () => {
+  docDB.open().then((db) => {
+    return db.collection("hnfeed_initial");
+  }).then((mcoll) => {
+    return mcoll.find({status: "pending"}).limit(10).toArray();
+  }).then((results) => {
+    docDB.close();
+    results.map((item) => {
+      fetch(item.url).then((response)=>{
+        if(response.status >= 400){
+          throw new Error("Got fucked....");
+        }
+        return response.json();
+      }).then((details) => {
+        updateitem(details);
+      }).catch((err)=>{
+        console.log("Error updating: ", err)
+      });
+    });
   }).catch((err) => {
-    console.log("error: ", err);
+    console.log("error while getting details: ", err);
   })
 }
 
-let inserthnids = function(mcollection, hnDoc) {
+let ifStory = (item, callback) => {
+  if(item.type == 'story'){
+    callback(null, item);
+  };
+}
+
+let insertinitiallist = (docs) => {
+  console.log("I am in insertinitialfeed function");
   docDB.open().then((db) => {
-    return db.collection(mcollection);
+    return db.collection("hnfeed_initial");
   }).then((mcoll) => {
     // console.log(checkIdExists(mcollection, hnDoc.id));
-    return mcoll.insertOne(hnDoc);
+    return mcoll.insertMany(docs);
   }).then((result) => {
     if(result){
     console.log("result: ", result.result.n);}
     docDB.close();
   }).catch((err) => {
-    console.log("error: ", err);
-  })
+    console.log("insertinitialfeed error :", err);
+  });
 }
 
-exports.getFeed = () => {
+let geturl = (id, callback) => {
+  let url = "https://hacker-news.firebaseio.com/v0/item/number.json?print=pretty".replace("number", id);
+  callback(null, {"hnid": id, "url": url, "status": "pending"});
+}
+
+exports.getinitialfeedlist = () => {
   fetch('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty')
   .then((response) => {
     if(response.status > 400){
@@ -39,32 +100,10 @@ exports.getFeed = () => {
     }
     return response.json();
   }).then((feed)=>{
-    feed.map((id)=>{
-      inserthnids("hnfeed_initial", {"id": id, "status": "pending"});
+    async.map(feed, geturl, (err, result) => {
+      insertinitiallist(result);
     });
   }).catch((err) => {
-    console.log(err.message);
+    console.log("tempfuncton error: ", err.message);
   });
-}
-
-exports.getIdDetails = () => {
-  docDB.open().then((db) => {
-    return db.collection("hnfeed_initial");
-  }).then((mcoll) => {
-    return mcoll.find({status: "pending"}).limit(10).toArray();
-  }).then((results) => {
-    results.map((item) => {
-      let url = "https://hacker-news.firebaseio.com/v0/item/number.json?print=pretty".replace("number", item.id);
-      fetch(url).then((response)=>{
-        if(response.status >= 400){
-          throw new Error("Got fucked");
-        }
-        return response.json();
-      }).then((details) => {
-        console.log(details);
-      });
-    });
-  }).catch((err) => {
-    console.log("error while getting details: ", err);
-  })
 }
