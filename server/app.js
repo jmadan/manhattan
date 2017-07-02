509a96a0c4caca019539a25017c5be03bc06f0be
Jasdeep Madan <jmadan@protonmail.com>
2017-07-02 23:15:35 +1000

    WIP -- using handlebars and express server to serve the article list

--- server/app.js
+++ server/app.js
@@ -1,17 +1,17 @@
 let cron = require('node-cron');
 let hnfeed = require('./modules/hnfeed');
 
-// cron.schedule('* * 6 * *', () => {
-//   console.log("initialing intial feed retrieval....");
-//   hnfeed.getinitialfeedlist();
-//  });
-//
-//  cron.schedule('* * * * *', () => {
-//    console.log("initialing feed deatil from HN....");
-//    hnfeed.getfeeditemdetails();
-// });
+cron.schedule('* * 6 * *', () => {
+  console.log("initialing intial feed retrieval....");
+  hnfeed.getinitialfeedlist();
+ });
 
-cron.schedule('* * * * *', () => {
+ cron.schedule('* 10 * * *', () => {
+   console.log("initialing feed deatil from HN....");
+   hnfeed.getfeeditemdetails();
+});
+
+cron.schedule('* 2 * * *', () => {
   console.log("getting the article body text....");
   hnfeed.gettext();
 })
