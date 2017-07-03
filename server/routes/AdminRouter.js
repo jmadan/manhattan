33e149e48eeffdc7a8ff16dd06b2df3b72680d1b
Jasdeep Madan <jmadan@protonmail.com>
2017-07-03 23:48:39 +1000

    WIP - implemented a view to represent a document for editing.

--- server/routes/AdminRouter.js
+++ server/routes/AdminRouter.js
@@ -14,12 +14,20 @@ router.get('/', (req, res) => {
 
 router.get('/all-articles', (req, res) => {
 	admin.getalldocuments(req.query.docs).then((result) => {
-		console.log("I am in*****", result.article_list.length);
 		if(result.error) {
 			res.render('error', {message: result.error});
 		} else{
 			res.render('admin', { article_list: result.article_list});
 		}
+	});
+});
+
+router.get('/article/:id', (req, res) => {
+	admin.getdocument(req.params.id).then((result) => {
+		if(result.error) {
+			res.render('error', {message: result.error});
+		}
+		res.render('article', {article: result.article_list});
 	});
 });
 
