de288b78fe5f3d5edc6357f0495095dab6cc6aa3
Jasdeep Madan <jasdeepm@gmail.com>
2017-07-10 00:54:17 +1000

    removed unsed code and changed cronjob timing to run every 6 hours to get hn feed

--- server/modules/cronjobs.js
+++ server/modules/cronjobs.js
@@ -1,7 +1,7 @@
 let cron = require('node-cron');
 let hnfeed = require('./hnfeed');
 
-exports.getinitialHNFeed = cron.schedule('31 * * * *', () => {
+exports.getinitialHNFeed = cron.schedule('* */6 * * *', () => {
   console.log("initializing intial feed retrieval.........", new Date().toUTCString());
   hnfeed.getinitialfeedlist();
  });
@@ -15,27 +15,3 @@ exports.getFeedItemBody = cron.schedule('*/5 * * * *', () => {
   console.log("fetching article body text.........", new Date().toUTCString());
   hnfeed.gettext();
 });
-
-
-// let hnfeed = require('./hnfeed');
-//
-// module.exports ={
-//   start: () => {
-//     console.log("printing this for everytime it runs", new Date().toUTCString());
-//   }
-// }
-
-// exports.getinitialHNFeed = cron.schedule('* * * * *', () => {
-//   console.log("initialing intial feed retrieval....");
-//   hnfeed.getinitialfeedlist();
-//  });
-//
-// exports.getHNFeedDetail = cron.schedule('* 10 * * *', () => {
-//    console.log("initialing feed detail from HN....");
-//    hnfeed.getfeeditemdetails();
-// });
-//
-// exports.getFeedItemBody = cron.schedule('* 2 * * *', () => {
-//   console.log("getting the article body text....");
-//   hnfeed.gettext();
-// });
