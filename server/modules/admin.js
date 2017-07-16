916001a575bacaf3c0601ea7161528b186f85efc
Jasdeep Madan <jasdeepm@gmail.com>
2017-07-16 13:42:56 +1000

    implemented update and delete document functions and changed the collection to fetch from

--- server/modules/admin.js
+++ server/modules/admin.js
@@ -1,24 +1,21 @@
 let docDB = require('./docDB');
 let Q = require('q');
 
-exports.getdocuments = (noofdocs, category) => {
+let getdocuments = (noofdocs, category) => {
   let deferred = Q.defer();
   if(!noofdocs){
     noofdocs = 1;
   }
   docDB.close();
-  docDB.open()
-  .then((db) => {
-    return db.collection("hnfeed_initial");
-  })
-  .then((collection) => {
-    return collection.find({status: "classification pending"}).limit(noofdocs).toArray();
-  })
-  .then((docs) => {
+  docDB.open().then((db) => {
+    return db.collection("feed");
+  }).then((collection) => {
+    return collection.find({'status': 'classification pending'}).limit(parseInt(noofdocs)).toArray();
+  }).then((docs) => {
     docDB.close();
     deferred.resolve({
       error: false,
-      article_list: docs
+      list: docs
     });
   }).catch((err)=>{
     deferred.reject({
@@ -29,11 +26,11 @@ exports.getdocuments = (noofdocs, category) => {
   return deferred.promise;
 }
 
-exports.getdocument = (id) => {
+let getdocument = (id) => {
   let deferred = Q.defer();
   docDB.close();
   docDB.open().then((db) => {
-    return db.collection("hnfeed_initial");
+    return db.collection("feed");
   })
   .then((collection) => {
     return collection.find({hnid: parseInt(id)}).limit(1).toArray();
@@ -42,7 +39,7 @@ exports.getdocument = (id) => {
     docDB.close();
     deferred.resolve({
       error: false,
-      article_list: docs
+      list: docs
     });
   }).catch((err) => {
     deferred.reject({
@@ -52,3 +49,49 @@ exports.getdocument = (id) => {
   });
   return deferred.promise;
 }
+
+let updatedocument = (hnid, category) => {
+  let deferred = Q.defer();
+  docDB.open().then((db) =>{
+    return db.collection('feed');
+  }).then((coll) => {
+    return coll.updateOne({hnid: parseInt(hnid)}, {$set: {classification: category, status: 'classified'}});
+  }).then((result)=>{
+    docDB.close();
+    deferred.resolve({
+      error: false,
+      message: "document updated",
+      docID: hnid
+    });
+  }).catch((err) =>{
+    deferred.reject({
+      error: true,
+      message: err
+    });
+  });
+  return deferred.promise;
+}
+
+let deletedocument = (hnid) => {
+  let deferred = Q.defer();
+  docDB.open().then((db) =>{
+    return db.collection('feed');
+  }).then((coll) => {
+    return coll.deleteOne({hnid: parseInt(hnid)});
+  }).then((result)=>{
+    docDB.close();
+    deferred.resolve({
+      error: false,
+      message: "document with ID "+ hnid +" deleted",
+      docID: hnid
+    });
+  }).catch((err) =>{
+    deferred.reject({
+      error: true,
+      message: err
+    });
+  });
+  return deferred.promise;
+}
+
+export {getdocument, getdocuments, updatedocument, deletedocument}
