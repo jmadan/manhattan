d1efcaedfccd16b58bf78c022f3bfbb79bb030cd
Jasdeep Madan <jasdeepm@gmail.com>
2017-07-16 13:53:39 +1000

    added routes for getting specific article, update and starting/stopping the jobs with the admin and article view

--- server/routes/adminrouter.js
+++ server/routes/adminrouter.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router();
-import admin from '../modules/admin';
+import * as admin from '../modules/admin';
+let hnfeed = require('../modules/hnfeed');
 import * as cronjob from '../modules/cronjobs';
 
 router.use((req, res, next) => {
@@ -12,6 +13,8 @@ router.use((req, res, next) => {
 router.get('/', (req, res) => {
 
 	let taskStatus={
+		'name': '',
+		'status': '',
 		'initialHNFeed': false,
 		'hnFeedDetail': false,
 		'feedItemBody': false
@@ -19,30 +22,44 @@ router.get('/', (req, res) => {
 
 	switch (req.query.task) {
 		case 'start':
-			console.log("starting all the cron jobs...");
+			console.log("Starting all the cron jobs...");
+			cronjob.getinitialHNFeed.start();
+			cronjob.getHNFeedDetail.start();
+			cronjob.getFeedItemBody.start();
+			break;
+		case 'stop':
+			console.log("Stopping all the cron jobs...");
+			cronjob.getinitialHNFeed.stop();
+			cronjob.getHNFeedDetail.stop();
+			cronjob.getFeedItemBody.stop();
 			break;
 		case 'startfeed':
-			console.log("starting the Job");
+			console.log("starting initial HN Feed job");
 			cronjob.getinitialHNFeed.start();
 			taskStatus.initialHNFeed = true;
 			break;
 		case 'stopfeed':
+			console.log("stopping initial HN Feed job");
 			cronjob.getinitialHNFeed.stop();
 			taskStatus.initialHNFeed = false;
 			break;
 		case 'startfeeddetail':
+			console.log("starting get HN Feed detail job");
 			cronjob.getHNFeedDetail.start();
 			taskStatus.hnFeedDetail = true;
 			break;
 		case 'stopfeeddetail':
+			console.log("stopping get initial HN Feed detail job");
 			cronjob.getHNFeedDetail.stop();
 			taskStatus.hnFeedDetail = false;
 			break;
 		case 'startfeeditembody':
+			console.log("starting Feed Item Body HN job");
 			cronjob.getFeedItemBody.start();
 			taskStatus.feedItemBody = true;
 			break;
 		case 'stopfeeditembody':
+			console.log("stopping Feed Item Body HN job");
 			cronjob.getFeedItemBody.stop();
 			taskStatus.feedItemBody = false;
 			break;
@@ -52,15 +69,16 @@ router.get('/', (req, res) => {
 	res.render('admin', {homepage: true, task_status: taskStatus});
 });
 
+//route - return list of articles based on the list limit and if they have been classified or not
 router.get('/articles', (req, res) => {
-	let cat = req.query.category;
-	let article_limit = req.query.limit;
-	admin.getdocuments(article_limit, cat).then((result) => {
-		if(result.error) {
-			res.render('error', {message: result.error});
-		} else{
-			res.render('admin', { article_list: result.article_list});
-		}
+	let category = req.query.category;
+	let list_limit = req.query.limit;
+	admin.getdocuments(list_limit, category).then((result) => {
+			if(result.error) {
+				res.render('error', {message: result.error});
+			} else{
+				res.render('admin', { article_list: result.list});
+			}
 	});
 });
 
@@ -69,7 +87,36 @@ router.get('/article/:id', (req, res) => {
 		if(result.error) {
 			res.render('error', {message: result.error});
 		}
-		res.render('article', {article: result.article_list});
+		res.render('article', {article: result.list});
+	});
+});
+
+router.get('/article/edit/:id', (req, res) => {
+	admin.getdocument(req.params.id).then((result) => {
+		if(result.error) {
+			res.render('error', {message: result.error});
+		}
+		res.render('article', {article: result.list});
+	});
+});
+
+router.get('/article/delete/:id', (req, res) => {
+	admin.deletedocument(req.params.id).then((result) => {
+		if(result.error) {
+			res.render('error', {message: result.error});
+		}
+		res.redirect('/admin/articles?limit=10&category=false');
+	});
+});
+
+router.post('/article', (req, res) => {
+	let articleId = req.body.articleId;
+	let category = req.body.category;
+	admin.updatedocument(articleId, category).then((response) => {
+		if(response.error != true){
+			let uri = '/admin/article/'+ articleId;
+			setTimeout(res.redirect(uri), 1000);
+		}
 	});
 });
 
