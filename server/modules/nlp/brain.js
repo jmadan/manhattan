be430818cc8044a2d130ae212b7e358aec137eef
Jasdeep Madan <jasdeepm@gmail.com>
2017-08-24 22:39:07 +1000

    WIP - converted the training data into vector and implemented method to convert a given doc into vector representation for prediction

--- server/modules/nlp/brain.js
+++ server/modules/nlp/brain.js
@@ -1,19 +1,51 @@
 "use strict"
 
 const brain = require('brain');
-let data = require('./trainingdata');
+let trainingdata = require('./trainingdata');
 let net = new brain.NeuralNetwork();
+const mimir = require('./mimir');
+const MongoClient = require('mongodb').MongoClient;
+const DBURI = process.env.MONGODB_URI;
 
+function vec_result(res, num_classes) {
+  var i = 0,
+  vec = [];
+  for (i; i < num_classes; i += 1) {
+    vec.push(0);
+  }
+  vec[res] = 1;
+  return vec;
+}
 
-let letsrunit = async() => {
-  let globalDictionary = await data.createDict();
-  // let tData = await data.trainingData();
-  // let brainData = [];
-  // tData.map((t)=>{
-  //   brainData.push([mimir.bow(t.stemwords, globalDictionary), t.category]);
-  // })
-  //
-  // console.log(brainData);
+function maxarg(array) {
+  return array.indexOf(Math.max.apply(Math, array));
+}
+
+let letsrunit = async(id) => {
+  let docs = await trainingdata.fetchDocs();
+  let dictionary = await trainingdata.createDict(docs);
+  let tData = await trainingdata.formattedData(docs, dictionary);
+
+  let ann_train = tData.map(function (pair) {
+    return {
+      input: pair[0],
+      output: vec_result(pair[1], 3)
+    };
+  });
+  net.train(ann_train);
+
+  MongoClient.connect(DBURI, (err, db)=>{
+    db.collection('feeditems').findOne({"status": "unclassified"},(err, item)=>{
+      if(err){
+        console.log(err);
+      } else{
+        console.log(item.url);
+        let predict = net.run(trainingdata.convertToVector(item, dictionary));
+        console.log(predict);
+        console.log([maxarg(predict)]);
+      }
+    });
+  })
 }
 
 letsrunit();
--- server/modules/nlp/trainingdata.js
+++ server/modules/nlp/trainingdata.js
@@ -6,27 +6,37 @@ const MongoClient = require("mongodb");
 const DBURI = process.env.MONGODB_URI;
 const mimir = require('./mimir');
 
-let fetchRawTrainingData = async() => {
+let fetchDocs = async() => {
   return await MongoDB.getDocuments("feeditems", {status:'classified'}).then((docs) => {
     return docs;
   }).catch(err=>console.log(err));
 }
 
-let trainingData = async() => {
-  let docs = await fetchRawTrainingData();
+let formattedData = async(docs, dict) => {
   let tdata = [];
-  await data.map((d) => {
-    tdata.push({input: mimir.dict(d.stemwords),output: d.category});
+  await docs.map((d) => {
+    if(d.stemwords){
+      tdata.push({input: mimir.bow(d.stemwords.toString(), dict),output: d.category});
+    } else {
+      tdata.push({input: mimir.bow(d.stemmed.toString(), dict),output: d.category});
+    }
   });
   return tdata;
 }
 
-let createDict = async() => {
-  let data = await fetchRawTrainingData();
-  let dict = data.reduce((prev, curr) => {
-    return prev.concat(curr.stemwords);
+let createDict = async(docs) => {
+  let dict = docs.reduce((prev, curr) => {
+    if(curr.stemwords){
+      return prev.concat(curr.stemwords);
+    } else{
+      return prev.concat(curr.stemmed);
+    }
   }, []);
   return mimir.dict(dict.toString());
 }
 
-module.exports = {fetchRawTrainingData, trainingData, createDict }
+let convertToVector = async(doc, dict) => {
+  return await mimir.bow(doc.stemwords.toString(), dict);
+}
+
+module.exports = {fetchDocs, formattedData, createDict, convertToVector }
