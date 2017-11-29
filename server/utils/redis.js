022de4ef2a23265bd540a356f2e485a97f612825
Jasdeep Madan <jasdeepm@gmail.com>
2017-11-29 11:05:51 +1100

    removed unwanted code and using redis url

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -1,19 +1,11 @@
 const redis = require('redis');
 const url = require('url');
 
-// var redisURL = url.parse(process.env.REDISCLOUD);
-// let redisURL = url.parse('redis://rediscloud:jN2RI5Ecsn4OlY8x@redis-10483.c12.us-east-1-4.ec2.cloud.redislabs.com:10483');
-let redisURL = url.parse('redis://h:p1eeb20d95a784fb9ad719c91da991614024154e473aeb6c2fd38836a30fb7f6b@ec2-34-233-181-119.compute-1.amazonaws.com:48649');
-// var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-// client.auth(redisURL.auth.split(":")[1]);
+let redisURL = url.parse(process.env.REDIS_URL);
 
-// client.on('connect', function() {
-//     console.log('connected');
-// });
-
-function redisConnect(){
-  let client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-  client.auth(redisURL.auth.split(":")[1]);
+function redisConnect() {
+  let client = redis.createClient(redisURL.port, redisURL.hostname, { no_ready_check: true });
+  client.auth(redisURL.auth.split(':')[1]);
   return client;
 }
 
@@ -31,7 +23,7 @@ function getRedis(key) {
   return new Promise((resolve, reject) => {
     let client = redisConnect();
     client.get(key, (err, reply) => {
-      if(err) {
+      if (err) {
         reject(err);
       }
       resolve(JSON.parse(reply));
@@ -46,11 +38,11 @@ function delRedis(key) {
   redisQuit(client);
 }
 
-function checkRedis (key) {
+function checkRedis(key) {
   return new Promise((resolve, reject) => {
     let client = redisConnect();
     client.exists(key, (err, reply) => {
-      if(err){
+      if (err) {
         reject(err);
       }
       if (reply === 1) {
@@ -60,13 +52,12 @@ function checkRedis (key) {
       }
     });
     redisQuit(client);
-  })
+  });
 }
-
 
 module.exports = {
   setRedis,
   getRedis,
   delRedis,
   checkRedis
-}
+};
