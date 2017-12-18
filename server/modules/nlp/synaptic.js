e24b7923f079270b56cf65ea0f9f392436599263
Jasdeep Madan <jasdeepm@gmail.com>
2017-12-18 16:04:16 +1100

    working on making NN more efficient

--- server/modules/nlp/synaptic.js
+++ server/modules/nlp/synaptic.js
@@ -145,7 +145,7 @@ let createNetwork = () => {
 };
 
 let getNetwork = () => {
-  return Redis.getRedis('SynapticBrain').then(NW => {
+  return Redis.getRedis('SynapticBrain1').then(NW => {
     if (!NW) {
       return createNetwork();
     } else {
@@ -212,7 +212,10 @@ let trainNetwork = async () => {
     });
     console.log('Iterations completed: ', i);
   }
-  Redis.setRedis('SynapticBrain', JSON.stringify(NW));
+  Redis.setRedis('SynapticBrain1', JSON.stringify(NW));
+  Mongo.insertDocument('brain', NW.toJSON()).then(res => {
+    console.log('Network saved in Mongo...', res.ops);
+  });
   console.log('Network Trained...');
 };
 
