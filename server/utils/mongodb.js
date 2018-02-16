f381c077e2d436396a2bcee62ba8dc75f82a9af0
Jasdeep Madan <jasdeepm@gmail.com>
2018-02-16 21:18:16 +1100

    Refactored the mongo connections to use connection pool

--- server/utils/mongodb.js
+++ server/utils/mongodb.js
@@ -1,125 +1,86 @@
-'use strict';
+"use strict";
 
-const MongoClient = require('mongodb');
+const MongoClient = require("mongodb");
 const ObjectID = MongoClient.ObjectID;
 const DBURI = process.env.MONGODB_URI;
 let db;
 
-function db_connect() {
-  MongoClient.connect(
-    DBURI,
-    {
-      poolSize: 10
-    },
-    (err, database) => {
-      if (err) {
-        throw err;
-      }
-      db = database;
+MongoClient.connect(
+  DBURI,
+  {
+    poolSize: 10
+  },
+  (err, client) => {
+    if (err) {
+      throw err;
     }
-  );
-}
-db_connect();
+    db = client.db("manhattan");
+  }
+);
 
 let insertManyDocuments = (coll, docs) => {
   return new Promise((resolve, reject) => {
-    MongoClient.connect(DBURI, (err, datab) => {
+    db.collection(coll).insertMany(docs, (err, result) => {
       if (err) {
         reject(err);
       }
-      datab
-        .db('manhattan')
-        .collection(coll)
-        .insertMany(docs, (err, result) => {
-          datab.close();
-          if (err) {
-            reject(err);
-          }
-          resolve(result);
-        });
+      resolve(result);
     });
   });
 };
 
 let insertDocument = (coll, doc) => {
   return new Promise((resolve, reject) => {
-    MongoClient.connect(DBURI, (err, datab) => {
+    db.collection(coll).insertOne(doc, (err, result) => {
       if (err) {
         reject(err);
       }
-      datab
-        .db('manhattan')
-        .collection(coll)
-        .insertOne(doc, (err, result) => {
-          datab.close();
-          if (err) {
-            reject(err);
-          }
-          resolve(result);
-        });
+      resolve(result);
     });
   });
 };
 
 let deleteDocument = (coll, doc) => {
   return new Promise((resolve, reject) => {
-    MongoClient.connect(DBURI, (err, datab) => {
+    db.collection(coll).deleteOne({ _id: ObjectID(doc._id) }, (err, result) => {
       if (err) {
         reject(err);
       }
-      datab
-        .db('manhattan')
-        .collection(coll)
-        .deleteOne({ _id: ObjectID(doc._id) }, (err, result) => {
-          datab.close();
-          if (err) {
-            reject(err);
-          }
-          resolve(result);
-        });
+      resolve(result);
     });
   });
 };
 
 let updateDocument = (coll, findQuery, updateQuery) => {
   return new Promise((resolve, reject) => {
-    MongoClient.connect(DBURI, (err, datab) => {
-      if (err) {
-        reject(err);
-      }
-      datab
-        .db('manhattan')
-        .collection(coll)
-        .findOneAndUpdate(findQuery, updateQuery, { returnOriginal: false }, (error, result) => {
-          datab.close();
+    db
+      .collection(coll)
+      .findOneAndUpdate(
+        findQuery,
+        updateQuery,
+        { returnOriginal: false },
+        (error, result) => {
           if (error) {
             reject(error);
           }
           resolve(result);
-        });
-    });
+        }
+      );
   });
 };
 
 let getDocuments = (coll, query, options) => {
   return new Promise((resolve, reject) => {
-    MongoClient.connect(DBURI, (err, datab) => {
-      if (err) {
-        reject(err);
-      }
-      datab
-        .db('manhattan')
-        .collection(coll)
-        .find(query, options)
-        .sort({ pubDate: -1 })
-        .toArray((err, docs) => {
-          if (err) {
-            reject(err);
-          }
-          datab.close();
-          resolve(docs);
-        });
-    });
+    db
+      .collection(coll)
+      .find(query, options)
+      .sort({ pubDate: -1 })
+      .toArray((err, docs) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(docs);
+      });
   });
 };
 
